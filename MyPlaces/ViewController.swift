d7840a4e873b4d7df2b0665ce0ffc4bbbf54d90a
Firdavs Bagirov <firdavsbagirov@gmail.com>
2020-10-22 15:28:35 +0500

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Firdavs Bagirov on 10/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
